10e888d8148b194071bb493a551d0d9cb362a235
Rytar33 <maks.Khromakov@mail.ru>
2024-04-10 07:13:25 +0300

    feat & fix: add automapper, dtos models, repository

---
+++ Application/MappingProfiles/PersonProfile.cs
@@ -0,0 +1,10 @@
+namespace Application.MappingProfiles;
+
+public class PersonProfile : Profile
+{
+    public PersonProfile()
+    {
+     
+        //TODO: Поресёрчить вложенные сущности автомаппинга
+    }
+}
